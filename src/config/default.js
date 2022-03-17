a593152e1dfc4117ab94d57054e65aed969c7389
pengzehua <zehua.peng@ninebot.com>
2022-03-17 20:52:35 +0800

    路径更换

--- src/config/default.js
+++ src/config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
   SHOP: 'https://godspen.ymm56.com/shop/',
-  VIEW_PATH: 'view',
-  ADMIN_PATH: 'admin',
+  VIEW_PATH: 'v',
+  ADMIN_PATH: 'ninebotadmineditor',
   EDITOR_PATH: 'editor',
   API_PATH: 'api',
   EDITOR_TITLE: '编辑器',
-  ADMIN_TITLE: '管理后台',
+  ADMIN_TITLE: 'Ninebot-Halo',
   BAIDU_TONGJI: ''
 }
